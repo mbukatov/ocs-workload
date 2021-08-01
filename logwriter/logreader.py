7a1f7e01ed34f9679ef44092210166cdf7ddf72d
Martin Bukatovič <mbukatov@redhat.com>
2021-08-01 21:24:40 +0200

    logerader script

---
+++ logwriter/logreader.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import argparse
+import hashlib
+import logging
+import sys
+
+
+def main():
+    ap = argparse.ArgumentParser(description="logreader")
+    ap.add_argument(
+        "logfile",
+        nargs='+',
+        type=argparse.FileType('r'),
+        help="filepath of a log file to read and verify")
+    ap.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="set log level to DEBUG")
+    args = ap.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    for fo in args.logfile:
+        logging.info("log file %s opened", fo.name)
+        prev_line = None
+        for line in fo:
+            timestamp, data = line.split()
+            if prev_line is not None:
+                checksum = hashlib.sha256(prev_line.encode('utf8')).hexdigest()
+                if checksum == data:
+                    logging.debug("line at %s verified", timestamp)
+                else:
+                    logging.error(
+                        "line at %s doesn't provide good digest",
+                        timestamp)
+            prev_line = line
+
+if __name__ == '__main__':
+    sys.exit(main())
